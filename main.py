2e4d075f8c696e5272282271bddd18047e116ef4
Tomasz Sas <sastomasz@gmail.com>
2021-12-11 09:05:23 +0100

    convert video to gray color and save first frame

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import cv2
 
+first_frame = None
+
 camera = cv2.VideoCapture(0)
 while True:
     check, frame = camera.read()
-    cv2.imshow("Captured video", frame)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    if first_frame is None: 
+        first_frame = gray_frame
+
+    cv2.imshow("Captured video", gray_frame)
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
